f90076ea1b768c9c70e10236d2318165e64f3641
Cecilesg <csgodefroy@gmail.com>
2018-08-13 19:01:31 +0200

    Updated Class Level to create a list of free sprites where in game objects can randomly spawn.

--- classes.py
+++ classes.py
@@ -10,68 +10,92 @@ from constants import *
 from random import *
 
 
-# TODO: create class Level to generate game level and display it from read file
+# TODO: create class Level to generate game level & display it from read file.
 class Level:
 	"""Class to create a level in the game."""
 	def __init__(self, file):
-		# Define attibute file (for lvl)
+		# Define attibute file (for lvl).
 		self.file = file
-		# Define attribute grid
+		# Define attribute grid.
 		self.grid = 0
+		# define attribute empty tuples list which runs twice: once for columns &--
+		# --once for lines (x,y) for the empty spots in the labyrinth where--
+		# --the in game objects can randomly spawn.
+		self.free = [] # free = free sprites.
 
-	# Method 1 = generates a game level according to lvl file in an attribute 'structure'
+	# Method 1 = generates a game level according to lvl file in an--
+	# --attribute 'structure'.
 	def generate(self):
 		"""Method which generates a game level according to the file read.
 		We create a general list containing a line per line list to display."""
-		# Open the file and read ('r') it
+		
+		# Open the file and read ('r') it.
 		with open(self.file, 'r') as file:
 			grid_level = []
-			# Run the lines in the file
+			# Initialize coordinates at -1 because the grid starts at 0--
+			# and I will append +1 to columns and lines.
+			x = -1
+			y = -1
+			# Run the lines in the file.
 			for line in file:
 				line_level = []
-				# Run each sprite/letters in the file
+				# Count lines in the grid.
+				y += 1
+				# A new line starts, columns restart.
+				x = -1
+				# Run each sprite/letters in the file.
 				for sprite in line:
+					# Count the columns in the grid.
+					x += 1 
 					# Ignore '\n' at the end of each line (not a sprite)
 					if line != '\n':
 						# Add sprite to lines list
 						line_level.append(sprite)
-				# Add lines to level list
+					# Check if sprite is a free spot in the grid.
+					if sprite == '0':
+						# If sprite is free add sprite to list free.
+						self.free.append((x,y))
+				# Add lines to level list.
 				grid_level.append(line_level)
-			# Save the grid
-			self.grid = grid_level 
+			# Save the grid.
+			self.grid = grid_level
+		# Print the structure of the grid in terminal.
+		print(self.grid)
 
-	# Method 2 = displays labyrinth aka game level (we only have one)
+	# Method 2 = displays labyrinth aka game level (we only have one).
 	def display(self, display):
-		"""Method which displays game level according to grid list returned by generate()."""
-		# Load images
+		"""Method which displays game level according to grid list returned
+		by generate()."""
+		
+		# Load images.
 		wall = pygame.image.load(image_wall).convert()
 		start = pygame.image.load(image_start).convert()
-		# convert_alpha for transparent images
+		# convert_alpha for transparent images.
 		end = pygame.image.load(image_end).convert_alpha()
 
-		# Run level list
+		# Run level list.
 		line_nb = 0
 		for line in self.grid:
-			# Run lines lists
+			# Run lines lists.
 			sprite_nb = 0
 			for sprite in line:
-				# Calculate real position in pixels per line
+				# Calculate real position in pixels per column.
 				x = sprite_nb * sprite_size
-				# Calculate real position in pixels per column
+				# Calculate real position in pixels per line.
 				y = line_nb * sprite_size
-				if sprite == 'w': # w = wall
+				if sprite == 'w': # w = wall.
 					display.blit(wall, (x,y))
-				elif sprite == 's': # s = start
+				elif sprite == 's': # s = start.
 					display.blit(start, (x,y))
-				elif sprite == 'e': # e = end = guardian.png = is transparent
+				elif sprite == 'e': # e = end = guardian.png = is transparent.
 					display.blit(end, (x,y))
-				# Add one sprite
+				# Add one sprite.
 				sprite_nb += 1
-			# Add one line
+			# Add one line.
 			line_nb += 1
 
 
-# TODO: create class Character to display MacGyver and make him move
+# TODO: create class Character to display MacGyver and make him move.
 class Character:
 	"""Class that creates a character."""
 	
