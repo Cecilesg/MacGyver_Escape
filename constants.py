f30c1154087e8d66bd6a904a992d0ac112b1e871
Cecilesg <csgodefroy@gmail.com>
2018-08-13 15:22:19 +0200

    Updated the constants with all the appropriate images destinations and names.

--- constants.py
+++ constants.py
@@ -5,25 +5,28 @@
 # TODO: create window display parameters for game.
 # Display window parameters =
 sprite_size = 40
-sprite_per_side = 20
+sprite_per_side = 15
 display_per_side = sprite_per_side * sprite_size
 
 # TODO: create game menu customization images list for import.
 # Game Menu customization =
-
 game_name = "MacGyver Escape"
-image_icon = "images/MacGyver.png"
+image_icon = "images/macgyver.png"
+image_game_menu = "images/game_menu.png"
 
 # TODO: create labyrinth game images list for import.
 # Game level background images =
-
+image_background = "images/background.png"
+image_start = "images/tile_start.png"
+image_wall = "images/tile_wall.png"
+image_end = "images/guardian.png"
 
 # TODO: create characters game images list for import.
 # Game Characters list =
-
+ig_macgyver = "images/macgyver.png"
+ig_murdoc = "images/guardian.png"
 
 # TODO: create object list
-
-
-
-
+ig_object1 = "images/object_circuit.png"
+ig_object2 = "images/object_chemicals.png"
+ig_object3 = "images/object_electricity.png"
